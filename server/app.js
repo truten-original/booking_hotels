3b6157499b42ab8ea9d7fcc3824c7216d065ac7d
Roman Trutnev <104026522+truten-original@users.noreply.github.com>
2022-12-12 11:09:22 +0300

    added docker file

--- server/app.js
+++ server/app.js
@@ -5,16 +5,28 @@ const config = require('config')
 const chalk = require('chalk')
 const initDataBase = require('./startUp/initDataBase')
 const routes = require('./routes')
+const path = require('path')
 const app = express()
 // if(process.env.NODE_ENV === 'production'){
 //     console.log("production")
 // } else {
 //     console.log('development')
 // }
+
+
 app.use(cors())
 app.use(express.json(), express.urlencoded({extended: false}))
 app.use('/api', routes)
 const PORT = config.get('port') ?? 8080
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client')))
+    const indexPath = path.join(__dirname, 'client', 'index.html')
+    app.get('*', (req, res) => {
+        res.sendFile(indexPath)
+    })
+}
+
 async function start () {
     try {
         mongoose.connection.once('open', () => {
